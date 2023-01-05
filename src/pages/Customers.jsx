978cc0024814eafda29428adad322ff5ec0f9e03
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-05 07:35:34 -0800

    add home page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function Customers() {
+  const [customers, setCustomers] = useState([])
+  
+  useEffect(()=>{
+    axios.get('https://northwind.vercel.app/api/customers').then((res)=>{setCustomers(res.data)})
+  },[])
+  console.log(customers)
+
   return (
     <div>Customers</div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import "./style.css";
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='container'>
+        <h1>HomePage</h1>
+        <div className='home_wrapper'>
+          <span>
+            <img src="https://wish-list-redux-next-js-3g4j.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhomepage.c7708c33.png&w=1080&q=75" alt="Biznes" />
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
